10e0ada3c49ba3e5e424fd1f1e8df7d0a7b8b448
Amir <rougetimelord@users.noreply.github.com>
2015-11-17 01:52:30 -0800

    Use webkit flags (RIP FF users)
    Support FF later

--- about.js
+++ about.js
@@ -14,13 +14,13 @@ var aboutFunc = function() {
       link.innerText="About";
       first = false;
     }
-    document.getElementById('disp').style = "transition: 1s ease-in-out;transform: scale(1);"
+    document.getElementById('disp').style = "-webkit-transition: 1s ease-in-out; -webkit-transform: scale(1);"
     document.getElementById('about').innerHTML="";
     shown=false;;
   }
   else
   {
-    document.getElementById('disp').style = "transition: 1s ease-in-out;transform: scale(.5);"
+    document.getElementById('disp').style = "-webkit-transition: 1s ease-in-out; -webkit-transform: scale(.5);"
     document.getElementById('about').innerHTML=st;
     shown = true;
   }
