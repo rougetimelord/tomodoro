f12d4740704d9835fb0195d4dcdf5b81eedacbc3
Rouge <sn.falconeye@yahoo.com>
2015-12-30 16:22:20 -0800

    Puts expiremental clock into actual effect

--- clock.js
+++ clock.js
@@ -1,8 +1,13 @@
-var m = 0, s = 15, r = 1, ds = "00", c = 0, br = !0, txt = "First grace period", start = 1; document.addEventListener("DOMContentLoaded", function () { "granted" !== Notification.permission && Notification.requestPermission() }); window.addEventListener("beforeunload", function (b) { if (!br) return "Are you really done with your work?" }); (function () { setInterval(function () { tick() }, 1E3) })(); function tick() { s--; 59 == s && m--; update() }
-function update() { 0 === m && 0 >= s && (0 === r && (alarm(txt), txt = "Grace period", m = 0, s = 15, r = 1), 1 == start && (alarm(txt), start = 0), 1 == r && 15 !== s && (br || (3 > c ? (txt = "Short break", m = 5) : (txt = "Long break", c = 0, m = 10), s = 60, c++, br = !0), br && 0 === m && (br = !1, txt = "Work time", m = 25, s = 60), r = 0), document.getElementById("txt").innerText = txt); format(); document.getElementById("disp").innerText = m + ":" + ds } function format() { switch (!0) { case 0 >= s && 0 !== m: s = 60; break; case 9 >= s: ds = String("0" + s); break; case 60 <= s: ds = "00"; break; default: ds = String(s) } }
-function alarm(b) {
-    var d = function () { document.getElementById("break").pause(); document.getElementById("work").pause(); document.getElementById("break").currentTime = 0; document.getElementById("work").currentTime = 0 }; b += " is over"; if ("granted" === Notification.permission) {
-        var a = new Notification(br ? "Time to get back to work" : "Time to take a break", { icon: "/tomodoro/Content/clock.png", body: b }); a.addEventListener("show", function () { setTimeout(a.close.bind(a), 2E3) }); a.addEventListener("click",
-        d); a.addEventListener("close", d)
-    } br ? document.getElementById("work").play() : document.getElementById("break").play(); setTimeout(d, 2E3)
-};
+(function () {
+    function h(a) {
+        function b() { document.getElementById("break").pause(); document.getElementById("work").pause(); document.getElementById("break").currentTime = 0; document.getElementById("work").currentTime = 0 } a += " is over"; if ("granted" === Notification.permission) {
+            var c = new Notification(d ? "Time to get back to work" : "Time to take a break", { icon: "/tomodoro/Content/clock.png", body: a }); c.addEventListener("show", function () { setTimeout(c.close.bind(c), 2E3) }); c.addEventListener("click", b); c.addEventListener("close",
+            b)
+        } d ? document.getElementById("work").play() : document.getElementById("break").play(); setTimeout(b, 2E3)
+    } var b = 0, a = 15, f = 1, e = "00", g = 0, d = !0, c = "First grace period", k = 1; document.addEventListener("DOMContentLoaded", function () { "granted" !== Notification.permission && Notification.requestPermission() }); window.addEventListener("beforeunload", function () { if (!d) return "Are you really done with your work?" }); (function () {
+        setInterval(function () {
+            a--; 0 === b && 0 >= a && (0 === f && (h(c), c = "Grace period", b = 0, a = 15, f = 1), 1 == k && (h(c),
+            k = 0), 1 == f && 15 !== a && (d || (3 > g ? (c = "Short break", b = 5) : (c = "Long break", g = 0, b = 10), a = 60, g++, d = !0), d && 0 === b && (d = !1, c = "Work time", b = 25, a = 60), f = 0), document.getElementById("txt").innerText = c); switch (!0) { case 0 >= a && 0 !== b: a = 60; break; case 9 >= a: e = String("0" + a); break; case 60 <= a: e = "00"; break; case 59 == a && 0 !== b: b--; e = "59"; break; default: e = String(a) } document.getElementById("disp").innerText = b + ":" + e
+        }, 1E3)
+    })()
+})();
