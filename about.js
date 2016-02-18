4786e04f670fae72e4c259576e911c50716be4e4
Rouge <sn.falconeye@yahoo.com>
2016-02-17 20:12:41 -0800

    Reverts service workers

--- about.js
+++ about.js
@@ -1,21 +1,3 @@
-// register service worker
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/tomodoro/sw.js', { scope: '/tomodoro/' }).then(function (reg) {
-
-        if (reg.installing) {
-            console.log('Service worker installing');
-        } else if (reg.waiting) {
-            console.log('Service worker installed');
-        } else if (reg.active) {
-            console.log('Service worker active');
-        }
-
-    }).catch(function (error) {
-        // registration failed
-        console.log('Registration failed with ' + error);
-    });
-};
 document.addEventListener('DOMContentLoaded', function ()
 {
     var shown = false;
