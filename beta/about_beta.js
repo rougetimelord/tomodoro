dc80dc1cbe08ab6cf7d150ce1c51910581d97361
Rouge <sn.falconeye@yahoo.com>
2016-03-17 17:33:14 -0700

    Pulls eee2f26 and 6cc11a7 to gh-pages

---
+++ beta/about_beta.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function ()
+{
+    var shown = false;
+    var link = document.getElementById('aboutLink');
+    link.addEventListener('click', function () {
+        var r = Math.floor((Math.random() * 10) + 1);
+        switch (r) {
+            case (1):
+                window.open("https://youtu.be/dQw4w9WgXcQ", '_blank');
+                break;
+            default:
+                (!shown) ? (about.show(), shown = true) : (about.hide(), shown = false);
+                break;
+        }
+    }, 'false')
+    var count = 0;
+    window.addEventListener("keyup", function (e) { count = keyPress(e, count); });
+    window.addEventListener('beforeunload', function (e) {
+        if (!br && running) {
+            e.returnValue = 'Are you really done with your work?';
+            return 'Are you really done with your work?';
+        }
+    });
+}
+)
+var about = (function(){
+    var first = true;
+    var st = "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals traditionally 25 minutes in length, separated by short breaks. These intervals are known as &quot;pomodoros&quot;, the plural in English of the Italian word pomodoro meaning &quot;tomato&quot;. The method is based on the idea that frequent breaks can improve mental agility.<br><ol><li>Decide on the task to be done<li>Set the pomodoro timer to n minutes (traditionally 25)<li>Work on the task until the timer rings; record with an x<li>Take a short break (3&#45;5 minutes)<li>After four pomodoros, take a longer break (15&#45;30 minutes)&quot;</ol><br>The creator and others encourage a low-tech approach, using a mechanical timer, paper and pencil. The physical act of winding up the timer confirms the user's determination to start the task; ticking externalises desire to complete the task; ringing announces a break. Flow and focus become associated with these physical stimuli.<br><br>This non physical (therefore inferior) version by Blair &quot;Rouge&quot;<br><a href=https://github.com/rougetimelord/tomodoro/ target=_blank>Source</a>";
+    return {
+        show: function () {
+            if (first) {
+                document.getElementById('aboutLink').innerText = "About (Click to close)";
+            }
+            document.getElementById('disp').style.fontSize = "1.5em";
+            document.getElementById('about').innerHTML = st;
+        },
+        hide: function () {
+            if (first) {
+                document.getElementById('aboutLink').innerText = "About";
+                first = false;
+            }
+            document.getElementById('disp').style.fontSize = "6em";
+            document.getElementById('about').innerHTML = "";
+        }
+    }
+}());
+var run = true;
+var flasher;
+function keyPress(event, c)
+{
+    var key = event.keyCode;
+    var need = (c <= 1) ? 38 : (c <= 3) ? 40 : (c == 4) ? 37 : (c == 5) ? 39 : (c == 6) ? 37 : (c == 7) ? 39 : (c == 8) ? 66 : (c == 9) ? 65 : 13;
+    if (key == need)
+        c++;
+    else
+        c = 0;
+    if (c == 11) {
+        if (run == true) {
+            var swischer = false;
+            flasher = setInterval(function () { var img = (swischer == false) ? "./Content/heman.gif" : "./Content/frog.png"; document.body.style.background = "url('" + img + "') repeat right top"; swischer = !swischer; }, 2500);
+        }
+        if(run == false) {
+            clearInterval(flasher);
+            document.body.style.background = "";
+        }
+        run = !run;
+        c = 0;
+    }
+    return c;
+}
---
+++ beta/clock_beta.js
@@ -0,0 +1,106 @@
+var br = true;
+var running = false;
+(function () {
+    var mute = false, muteSt;
+    var m = 0;
+    var s = 15;
+    var r = 1;
+    var ds = "00";
+    var c = 0;
+    var txt = "First grace period";
+    var start = 1;
+    (function () {
+        if (Notification.permission !== "granted")
+            Notification.requestPermission();
+        muteSt = document.getElementById('muteSt');
+        document.getElementById('mute').addEventListener('click', function () { mute = !mute; (mute) ? (muteSt.innerHTML = 'Muted', muteSt.style.marginBottom = ".65em") : (muteSt.innerHTML = '', muteSt.style.marginBottom = "1.75em") });
+        document.getElementById('toggle').addEventListener('click', function () { var btn = document.getElementById('toggle'); if (!running) { interval = setInterval(function () { tick() }, 1000); btn.style.content = "url(./Content/pause.png)"; } else { clearInterval(interval); btn.style.content = "url(./Content/play.png)"; } running = !running; btn.classList.toggle('pause'); btn.classList.toggle('play') });
+    }());
+    function tick() {
+        s--;
+        update();
+    }
+    function update() {
+        if (m === 0 && s <= 0) {
+            if (r === 0) {
+                alarm(txt);
+                txt = "Grace period";
+                m = 0;
+                s = 15;
+                r = 1;
+            }
+            if (start == 1) {
+                alarm(txt);
+                start = 0;
+            }
+            if (r == 1 && s !== 15) {
+                if (!br) {
+                    if (c < 3) {
+                        txt = "Short break";
+                        m = 5;
+                    }
+                    else {
+                        txt = 'Long break';
+                        c = 0;
+                        m = 10;
+                    }
+                    s = 60;
+                    c++;
+                    br = true;
+                }
+                if (br && m === 0) {
+                    br = false;
+                    txt = 'Work time';
+                    m = 25;
+                    s = 60;
+                }
+                r = 0;
+            }
+            document.getElementById('txt').innerText = txt;
+        }
+        format();
+        document.getElementById('disp').innerText = m + ':' + ds;
+    }
+    function format() {
+        switch (true) {
+            case (s <= 0 && m !== 0):
+                s = 60;
+                break;
+            case (s <= 9):
+                ds = String("0" + s);
+                break;
+            case (s >= 60):
+                ds = String("00");
+                break;
+            case (s == 59 && m !== 0):
+                m--;
+                ds = '59';
+                break;
+            default:
+                ds = String(s);
+                break;
+        }
+    }
+    function alarm(a) {
+        var stop = function () {
+            document.getElementById('break').pause();
+            document.getElementById('work').pause();
+            document.getElementById('break').currentTime = 0;
+            document.getElementById('work').currentTime = 0;
+        };
+        var ntxt = a + " is over";
+        if (Notification.permission === "granted") {
+            var note = new Notification((!br) ? "Time to take a break" : "Time to get back to work", { icon: '/tomodoro/Content/clock.png', body: ntxt });
+            note.addEventListener('show', function () { setTimeout(note.close.bind(note), 2000) });
+            note.addEventListener('click', stop);
+            note.addEventListener('close', stop);
+        }
+        if (!mute) {
+            if (!br)
+                document.getElementById('break').play();
+            else
+                document.getElementById('work').play();
+            setTimeout(stop, 2000);
+        }
+    }
+}());
