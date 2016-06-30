cc344662ee4579292c12ce58592f1c085f0c1a65
Rouge <rougetimelord@users.noreply.github.com>
2016-06-30 14:02:06 -0700

    Fix paths

--- beta/sw.js
+++ beta/sw.js
@@ -2,23 +2,22 @@ this.addEventListener('install', function (event) {
     event.waitUntil(
       caches.open('v1').then(function (cache) {
           return cache.addAll([
-            './',
             './about.js',
             './clock.js',
             './index.css',
-            './Content/break.mp3',
-            './Content/work.mp3',
-            './Content/icon.png',
-            './Content/clock.png',
-            './Content/mute.png',
-            './Content/play.png',
-            './Content/pause.png',
-            './Content/mute_hover.png',
-            './Content/play_hover.png',
-            './Content/pause_hover.png',
-            './Content/mute_active.png',
-            './Content/play_active.png',
-            './Content/pause_active.png'
+            '/Content/break.mp3',
+            '/Content/work.mp3',
+            '/Content/icon.png',
+            '/Content/clock.png',
+            '/Content/mute.png',
+            '/Content/play.png',
+            '/Content/pause.png',
+            '/Content/mute_hover.png',
+            '/Content/play_hover.png',
+            '/Content/pause_hover.png',
+            '/Content/mute_active.png',
+            '/Content/play_active.png',
+            '/Content/pause_active.png'
           ]);
       })
     );
